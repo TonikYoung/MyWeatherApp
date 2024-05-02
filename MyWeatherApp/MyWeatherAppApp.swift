6f749205d994b69d58a7cd57b6b2a3b0e3328def
TonikYoung <nextprogress@gmail.com>
2024-05-02 23:41:01 +0300

    Initial Commit

---
+++ MyWeatherApp/MyWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWeatherAppApp.swift
+//  MyWeatherApp
+//
+//  Created by Anton Abramov on 02.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct MyWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
