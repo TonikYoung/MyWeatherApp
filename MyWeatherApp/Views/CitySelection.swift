25041353e660de6639e588de60fc3d535c0aeaf0
TonikYoung <norton5@yandex.ru>
2024-05-13 16:49:20 +0300

    test

---
+++ MyWeatherApp/Views/CitySelection.swift
@@ -0,0 +1,18 @@
+//
+//  CitySelection.swift
+//  MyWeatherApp
+//
+//  Created by abramovanto on 13.05.2024.
+//
+
+import SwiftUI
+
+struct CitySelection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CitySelection()
+}
