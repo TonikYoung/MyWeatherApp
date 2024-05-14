f1a2b2046b150d1a7f6fcca54790b7cbbb887b02
TonikYoung <norton5@yandex.ru>
2024-05-14 10:30:01 +0300

    New view added

--- MyWeatherApp/Views/CitySelection.swift
+++ MyWeatherApp/Views/CitySelection.swift
@@ -8,11 +8,60 @@
 import SwiftUI
 
 struct CitySelection: View {
+    
+    @Binding var backgroundColor: Color
+    @ObservedObject var viewModel = CityListViewModel()
+    @State private var userInput = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        NavigationView {
+            VStack {
+                TextField("Введите название города", text: $userInput)
+                    .textFieldStyle(PlainTextFieldStyle())
+                    .background(
+                        Rectangle()
+                            .foregroundColor(.white.opacity(0.2))
+                            .cornerRadius(25)
+                            .frame(height: 50)
+                    )
+                    .padding(.leading, 20)
+                    .padding(.trailing, 5)
+                    .padding(.bottom, 15)
+                    .padding(.top, 15)
+                    .multilineTextAlignment(.center)
+                    .accentColor(.white)
+                    .font(Font.system(size: 20, design: .default))
+                
+                    .onSubmit {
+                        viewModel.add(name: userInput)
+                        userInput = ""
+                    }
+                
+                List {
+                    ForEach(viewModel.cities) { city in
+                        Text(city.cityName)
+                    }
+                    .onDelete(perform: viewModel.delete(index:))
+                    .listRowBackground(Color.white.blur(radius: 75).opacity(0.5))
+                }
+                .contentMargins(.vertical, 0)
+                .scrollContentBackground(.hidden)
+                .preferredColorScheme(.dark)
+                
+                
+            }
+
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+            .background(backgroundColor)
+        }
+
     }
 }
 
+
+
+
 #Preview {
-    CitySelection()
+    CitySelection(backgroundColor: .constant(.blue))
 }
